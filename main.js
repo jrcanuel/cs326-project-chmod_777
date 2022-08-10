6f5ff7f623e6b510badee556e238b31e902674d3
Jason Canuel <jrcanuel@umass.edu>
2022-08-10 19:32:35 -0400

    left and right button ui, image

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ import { Menu } from './menu.js';
 import { Calculator } from './calculator.js';
 
 //main file / script, handles creating eventListeners
-
+let firstClick = true;
 let firstSelect = true;
 document.getElementById("recipeMenuBtn").addEventListener("click", () => {
+    if(firstClick) {document.getElementById("initialPic").remove();}
     let menu = new Menu();
+    firstClick = false;
 });
 
 document.getElementById("drinkCalcBtn").addEventListener("click", () => {
+    if(firstClick) {document.getElementById("initialPic").remove();}
     let calc = new Calculator();
+    firstClick = false;
 });
--- menu.js
+++ menu.js
@@ -4,7 +4,7 @@ import { Drink } from './drink.js';
 
 class Menu {
     constructor() {
-        this.drinks = [ //todo: array of 20 popular drink objects
+        this.drinks = [ //todo: array of 20 popular drink objects, change to an imported array from drink.js & add to calculator.js 
             new Drink("Dirty Shirley", ["sprite", "grenadine", "vodka"]),
             new Drink("Grateful Dead", ["tequila", "gin", "vodka", "rum", "raspberry liquer", "triple sec", "sour mix"])
         ];
@@ -27,9 +27,28 @@ class Menu {
         recipes.classList.add("recipesMenu");
         recipes.setAttribute("id", "recipes");
         document.getElementById("recipeMenu").appendChild(recipes);
+        console.log(this.drinks);
+
+        let leftButton = document.createElement("button");
+        leftButton.type = "button";
+        leftButton.innerHTML = "< Previous";
+        leftButton.classList.add("leftButton");
+        leftButton.setAttribute("id", "leftButton");
+        leftButton.style.marginTop = '10px';
+        leftButton.style.marginRight = '10px';
+        document.getElementById("recipeMenu").appendChild(leftButton);
+
+        let rightButton = document.createElement("button");
+        rightButton.type = "button";
+        rightButton.innerHTML = "Next >";
+        rightButton.classList.add("rightButton");
+        rightButton.setAttribute("id", "rightButton");
+        rightButton.style.marginTop = '10px';
+        rightButton.style.marginLeft = '10px';
+        document.getElementById("recipeMenu").appendChild(rightButton);
     }
 
-    filterAlphabetical() { //arrange drinks array into alphabetical order
+    filterAlphabetical() { //arrange drinks array into alphabetical order... filter select implementation todo.
 
     }
 
