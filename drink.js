9f55245d4c14f3a8d15c301875443f82feba8492
Jason Canuel <jrcanuel@umass.edu>
2022-08-10 18:18:47 -0400

    slightly updated calc / recipe menu visuals, js functions added to both

--- drink.js
+++ drink.js
@@ -1,8 +1,9 @@
 //Drink class, potentially to be used to create drink objects for better communication between menu & calc.
 
 class Drink {
-    constructor() {
-        this.ingredients = [] //array with ingredients; simpler to search for index than key of object imo, and no value needed other than title.
+    constructor(title, ingredients) {
+        this.title = title;
+        this.ingredients = ingredients //array with ingredients; simpler to search for index than key of object imo, and no value needed other than title.
     }
 }
 
--- menu.js
+++ menu.js
@@ -4,7 +4,11 @@ import { Drink } from './drink.js';
 
 class Menu {
     constructor() {
-        this.drinks = []    //todo: array of drink objects
+        this.drinks = [ //todo: array of 20 popular drink objects
+            new Drink("Dirty Shirley", ["sprite", "grenadine", "vodka"]),
+            new Drink("Grateful Dead", ["tequila", "gin", "vodka", "rum", "raspberry liquer", "triple sec", "sour mix"])
+        ];
+        this.currentDrink = 0;
         
         if(document.getElementById("drinkCalcBtn").classList.contains("disabled")) {
             document.getElementById("drinkCalcBtn").classList.remove("disabled");
@@ -23,6 +27,38 @@ class Menu {
         recipes.classList.add("recipesMenu");
         recipes.setAttribute("id", "recipes");
         document.getElementById("recipeMenu").appendChild(recipes);
+    }
+
+    filterAlphabetical() { //arrange drinks array into alphabetical order
+
+    }
+
+    //todo: add other filter types
+
+    switchRight() { //buttons on left and right to switch between available drinks
+        this.derender(this.drinks[currentDrink]);
+
+        if(this.currentDrink < this.drinks.length-1) {this.currentDrink++;}
+        else {this.currentDrink = 0;}
+
+        this.renderDrink(this.drinks[currentDrink]);
+    }
+
+    switchLeft() {
+        this.derender(this.drinks[currentDrink]);
+
+        if(this.currentDrink > 0) {this.currentDrink--;}
+        else {this.currentDrink = this.currentDrink.length-1;}
+
+        this.renderDrink(this.drinks[currentDrink]);
+    }
+
+    renderDrink() { //todo: implement code to render this.currentDrink
+        
+    }
+
+    derender() { //clean up elements created by render drink
+
     }
 }
 
