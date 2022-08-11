ab75412b9525fdc4e98d45d19a2e541d841e10b6
Jason Canuel <jrcanuel@umass.edu>
2022-08-10 20:48:21 -0400

    added example bev pics, rendering under recipe menu works, buttons work as well.

--- drink.js
+++ drink.js
@@ -1,9 +1,10 @@
 //Drink class, potentially to be used to create drink objects for better communication between menu & calc.
 
 class Drink {
-    constructor(title, ingredients) {
+    constructor(title, ingredients, pic) {
         this.title = title;
         this.ingredients = ingredients //array with ingredients; simpler to search for index than key of object imo, and no value needed other than title.
+        this.pic = pic;
     }
 }
 
--- menu.js
+++ menu.js
@@ -5,11 +5,12 @@ import { Drink } from './drink.js';
 class Menu {
     constructor() {
         this.drinks = [ //todo: array of 20 popular drink objects, change to an imported array from drink.js & add to calculator.js 
-            new Drink("Dirty Shirley", ["sprite", "grenadine", "vodka"]),
-            new Drink("Grateful Dead", ["tequila", "gin", "vodka", "rum", "raspberry liquer", "triple sec", "sour mix"]),
-            new Drink("Long Island Iced Tea", ["vodka", "rum", "tequila", "gin", "triple sec", "lemon juice", "coke"])
+            new Drink("Dirty Shirley", ["Lemon-Lime Soda", "Grenadine", "Vodka"], "https://s.yimg.com/ny/api/res/1.2/BVky3owZzXDoSnibOAw8Qg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MA--/https://s.yimg.com/uu/api/res/1.2/R6vY9szGtXPVLYPKrlh2vw--~B/aD0wO3c9MDthcHBpZD15dGFjaHlvbg--/https://media.zenfs.com/en/purewow_185/f51ae90008d571aa78f1d8d8106c4a12"),
+            new Drink("Grateful Dead", ["Tequila", "Gin", "Vodka", "Rum", "Raspberry Liquer", "Triple Sec", "Sour Mix"], "https://fullformtoday.com/wp-content/uploads/2022/03/Blue-Grateful-Dead-Drink-Recipe.jpg"),
+            new Drink("Long Island Iced Tea", ["Vodka", "Rum", "Tequila", "Gin", "Triple Sec", "Lemon Juice", "Cola"], "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/long-island-iced-tea-7-1650298273.jpg?crop=1.00xw:1.00xh;0,0&resize=640:*")
         ];
         this.currentDrink = 0;
+        var lastDrink;
         
         if(document.getElementById("drinkCalcBtn").classList.contains("disabled")) {
             document.getElementById("drinkCalcBtn").classList.remove("disabled");
@@ -56,6 +57,8 @@ class Menu {
         document.getElementById("rightButton").addEventListener("click", () => {
             this.switchRight();
         })
+
+        this.renderDrink();
     }
 
     filterAlphabetical() { //arrange drinks array into alphabetical order... filter select implementation todo.
@@ -65,9 +68,11 @@ class Menu {
     //todo: add other filter types
 
     switchRight() { //buttons on left and right to switch between available drinks
-        this.derender(this.drinks[this.currentDrink]);
+        this.derender(this.drinks[this.currentDrink].title);
+
         if(this.currentDrink < this.drinks.length-1) {this.currentDrink++;}
         else {this.currentDrink = 0;}
+
         this.renderDrink(this.drinks[this.currentDrink]);
 
         console.log("Drink " + this.currentDrink + ":");
@@ -75,23 +80,49 @@ class Menu {
     }
 
     switchLeft() {
-        this.derender(this.drinks[this.currentDrink]);
+        this.derender(this.drinks[this.currentDrink].title);
+
         if(this.currentDrink > 0) {this.currentDrink--;}
         else {this.currentDrink = this.drinks.length-1;}
+        
         this.renderDrink(this.drinks[this.currentDrink]);
 
         console.log("Drink " + this.currentDrink + ":");
         console.log(this.drinks[this.currentDrink]);
     }
 
-    renderDrink() { //todo: implement code to render this.currentDrink
+    renderDrink() { //todo: contain drink title, image, and recipe into a container, and remove that container in derender; this will make this simpler.
+        //title
         let curr = this.drinks[this.currentDrink]
         let drinkRecipe = document.createElement("div");
-        drinkRecipe.innerHTML = "<h1>" + this.drinks[this.currentDrink].title
+        drinkRecipe.style.color = "green";
+        drinkRecipe.setAttribute("id", curr.title);
+        drinkRecipe.innerHTML = "<br><h1>" + curr.title + ":</h1>";
+        document.getElementById("recipes").appendChild(drinkRecipe);
+
+        //image
+        let drinkPic = document.createElement("img");
+        drinkPic.setAttribute("id", curr.title + " pic");
+        drinkPic.setAttribute("src", curr.pic);
+        drinkPic.setAttribute("alt", "Image of the Beverage")
+        document.getElementById("recipes").appendChild(drinkPic);
+
+        //ingredients                                                       //figure out why <h1> isnt working & fix it.
+        let drinkIngredients = document.createElement("div");
+        drinkIngredients.setAttribute("id", curr.title + " ingredients");
+        drinkIngredients.innerHTML = "<h1>";
+        curr.ingredients.forEach(ingredient => {
+            drinkIngredients.innerHTML += ingredient
+            if(ingredient != curr.ingredients[curr.ingredients.length-1]) {drinkIngredients.innerHTML += ", "}
+        });
+        drinkIngredients.innerHTML += "</h1>";
+        document.getElementById("recipes").appendChild(drinkIngredients);
     }
 
-    derender() { //clean up elements created by render drink
-
+    derender(elementID) { //clean up elements created by render drink
+        document.getElementById(elementID).remove();
+        document.getElementById(elementID + " pic").remove();
+        document.getElementById(elementID + " ingredients").remove();
     }
 }
 
