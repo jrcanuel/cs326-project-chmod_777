064de203654d08b5f262246486585c580f20b5b5
Jason Canuel <jrcanuel@umass.edu>
2022-08-23 20:26:16 -0400

    relabled some stuff, fixed up database functions

--- server/database.js
+++ server/database.js
@@ -2,37 +2,38 @@ import {readFile, writeFile} from 'fs/promises';
 
 class Database {
     constructor() {
-        this.path = 'drinks.json';
+        this.D = 'drinks.json'; 
+        this.CI = 'currIngredients.json'; 
     }
 
     async addIngredient(ingredient) {
-        const data = await this.read();
+        const data = await this.read(this.CI);
         data.push(ingredient);
-        await this.write(data);
+        await this.write(data, this.CI);
     }
 
     async removeIngredient(ingredient) {
-        const data = await this.read();
+        const data = await this.read(this.CI);
         data = data.filter(element != ingredient);
-        await this.write(data);
+        await this.write(data, this.CI);
     }
 
     async getDrinks() {
-        const data = await this.read();
+        const data = await this.read(this.D);
         return data;
     }
 
-    async read() {
+    async read(path) {
         try {
-        const data = await readFile(this.path, 'utf8');
+        const data = await readFile(path, 'utf8');
         return JSON.parse(data);
         } catch (error) {
         return {ingredients: []};
         }
     }
 
-    async write(data) {
-        await writeFile(this.path, JSON.stringify(data), 'utf8');
+    async write(data, path) {
+        await writeFile(path, JSON.stringify(data), 'utf8');
     }
 }
 
